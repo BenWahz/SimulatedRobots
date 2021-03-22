a5eddefa6a31cc982fe45b6377aaeb66bca5a6d9
Henry Rice <hmrice@uvm.edu>
2021-03-22 19:17:00 -0400

    automated generation and simulation with a neural network with randomly generated weights

--- generate.py
+++ generate.py
@@ -1,6 +1,6 @@
 import pyrosim.pyrosim as pyrosim
 #pyrosim.Start_SDF("world.sdf")
-
+import random
 
 
 def Create_World():
@@ -34,15 +34,43 @@ def Generate_Brain():
     pyrosim.Send_Sensor_Neuron(name=2, linkName="Back_Leg")
     pyrosim.Send_Motor_Neuron(name=3, jointName="Torso_BackLeg")
     pyrosim.Send_Motor_Neuron(name=4, jointName="Torso_FrontLeg")
-    pyrosim.Send_Synapse(sourceNeuronName=0, targetNeuronName=3, weight=-1.0)
-    pyrosim.Send_Synapse(sourceNeuronName=1, targetNeuronName=3, weight=-0.25)
-    pyrosim.Send_Synapse(sourceNeuronName=2, targetNeuronName=3, weight=-0.75)
-    pyrosim.Send_Synapse(sourceNeuronName=0, targetNeuronName=4, weight=-1.0)
-    pyrosim.Send_Synapse(sourceNeuronName=1, targetNeuronName=4, weight=-0.75)
-    pyrosim.Send_Synapse(sourceNeuronName=2, targetNeuronName=4, weight=-0.25)
+    # pyrosim.Send_Synapse(sourceNeuronName=0, targetNeuronName=3, weight=-1.0)
+    # pyrosim.Send_Synapse(sourceNeuronName=1, targetNeuronName=3, weight=-0.25)
+    # pyrosim.Send_Synapse(sourceNeuronName=2, targetNeuronName=3, weight=-0.75)
+    # pyrosim.Send_Synapse(sourceNeuronName=0, targetNeuronName=4, weight=-1.0)
+    # pyrosim.Send_Synapse(sourceNeuronName=1, targetNeuronName=4, weight=-0.75)
+    # pyrosim.Send_Synapse(sourceNeuronName=2, targetNeuronName=4, weight=-0.25)
+    for i in range(3):
+        for j in range(3,5):
+            pyrosim.Send_Synapse(sourceNeuronName= i, targetNeuronName= j, weight= random.uniform(-1.0,1.0))
     pyrosim.End()
 
 
 Create_World()
 Generate_Body()
 Generate_Brain()
+
+# < synapse
+# sourceNeuronName = "0"
+# targetNeuronName = "3"
+# weight = "0.8449776935080169" / >
+# < synapse
+# sourceNeuronName = "0"
+# targetNeuronName = "4"
+# weight = "0.11088676104484896" / >
+# < synapse
+# sourceNeuronName = "1"
+# targetNeuronName = "3"
+# weight = "0.16678167595099103" / >
+# < synapse
+# sourceNeuronName = "1"
+# targetNeuronName = "4"
+# weight = "-0.5993838704797643" / >
+# < synapse
+# sourceNeuronName = "2"
+# targetNeuronName = "3"
+# weight = "-0.5645928821073778" / >
+# < synapse
+# sourceNeuronName = "2"
+# targetNeuronName = "4"
+# weight = "0.2332464281693174" / >
